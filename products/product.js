57af470e20e67b6afaa69705675eacdb17b5cf91
Arnav <76613041+VOTMAN@users.noreply.github.com>
2023-12-17 05:23:31 +0530

    Add Coins Feature

--- products/product.js
+++ products/product.js
@@ -104,61 +104,71 @@ const product = [
     },
 ];
 
-const categories = [...new Set(product.map((item)=>
-    {return item}))]
-    let i=0;
-document.getElementById('root').innerHTML = categories.map((item)=>
-{
-    var {image, title, price} = item;
-    return(
+const categories = [...new Set(product.map((item) => item.title))];
+let i = 0;
+
+document.getElementById('root').innerHTML = categories.map((title) => {
+    const item = product.find((productItem) => productItem.title === title);
+    return (
         `<div class='box'>
             <div class='img-box'>
-                <img class='images' src=${image}></img>
+                <img class='images' src=${item.image}></img>
             </div>
-        <div class='bottom'>
-        <p>${title}</p>
-        <h2>$ ${price}.00</h2>`+
-        "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
-        `</div>
+            <div class='bottom'>
+                <p>${item.title}</p>
+                <h2>$ ${item.price}.00</h2>
+                <button onclick='addtocart("${item.title}")'>Add to cart</button>
+            </div>
         </div>`
-    )
-}).join('')
+    );
+}).join('');
 
-var cart =[];
+var cart = [];
+var coins = 0;
 
-function addtocart(a){
-    cart.push({...categories[a]});
+function addtocart(title) {
+    const selectedItem = product.find((item) => item.title === title);
+    cart.push({ ...selectedItem });
+    coins += 10;
     displaycart();
-}
-function delElement(a){
-    cart.splice(a, 1);
-    displaycart();
+    displayCoins();
 }
 
-function displaycart(){
-    let j = 0, total=0;
-    document.getElementById("count").innerHTML=cart.length;
-    if(cart.length==0){
+function delElement(index) {
+    coins -= 10;
+    cart.splice(index, 1);
+    displaycart();
+    displayCoins();
+}
+
+function displaycart() {
+    let total = 0;
+    document.getElementById("count").innerHTML = cart.length;
+    if (cart.length == 0) {
         document.getElementById('cartItem').innerHTML = "Your cart is empty";
-        document.getElementById("total").innerHTML = "$ "+0+".00";
-    }
-    else{
-        document.getElementById("cartItem").innerHTML = cart.map((items)=>
-        {
-            var {image, title, price} = items;
-            total=total+price;
-            document.getElementById("total").innerHTML = "$ "+total+".00";
-            return(
+        document.getElementById("total").innerHTML = "$ " + 0 + ".00";
+    } else {
+        document.getElementById("cartItem").innerHTML = cart.map((items, index) => {
+            var { image, title, price } = items;
+            total = total + price;
+            document.getElementById("total").innerHTML = "$ " + total.toFixed(2);
+            return (
                 `<div class='cart-item'>
-                <div class='row-img'>
-                    <img class='rowimg' src=${image}>
-                </div>
-                <p style='font-size:12px;'>${title}</p>
-                <h2 style='font-size: 15px;'>$ ${price}.00</h2>`+
-                "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
+                    <div class='row-img'>
+                        <img class='rowimg' src=${image}>
+                    </div>
+                    <p style='font-size:12px;'>${title}</p>
+                    <h2 style='font-size: 15px;'>$ ${price}.00</h2>
+                    <i class='fa-solid fa-trash' onclick='delElement(${index})'></i>
+                </div>`
             );
         }).join('');
     }
-
-    
 }
+
+function displayCoins() {
+    document.getElementById("coins").innerText = coins;
+}
+
+// Initial display of coins
+displayCoins();
