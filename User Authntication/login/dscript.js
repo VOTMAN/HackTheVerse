8c8ef624f8bbb0d252858422432dac7f0a1f7018
Arnav <76613041+VOTMAN@users.noreply.github.com>
2023-12-17 11:51:44 +0530

    Made changes to login page

--- User Authntication/login/dscript.js
+++ User Authntication/login/dscript.js
@@ -2,11 +2,22 @@ document.addEventListener("DOMContentLoaded", function () {
     const loginForm = document.getElementById("loginForm");
     const registerNow = document.getElementById("registerNow");
     const forgotPassword = document.getElementById("forgotPassword");
+    let user = document.getElementById("username");
+    let pass = document.getElementById("password");
+    let loginText = document.querySelector(".text");
 
+    
+    
     loginForm.addEventListener("submit", function (event) {
         event.preventDefault();
         // Add your login logic here
-        alert("Login logic goes here!");
+        if (user.value == "admin" && pass.value == "admin") {
+            loginText.classList.remove("hide");
+            alert("Logged IN");
+        } else {
+            alert("Wrong password or username");
+        }
+        
     });
 
     registerNow.addEventListener("click", function (event) {
